95b6248492ff0e53941386fda247cff3443d705f
rootel <ckcksdl135@naver.com>
2023-12-12 13:38:16 +0900

    MintNFT & Layout Setting

---
+++ README.md
@@ -0,0 +1,3 @@
+# PFP Project
+
+Contract Address - 0xadf461b4bf91538b79e7f26faa90df87ccc330d4
---
+++ frontend/src/pages/sale.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Sale: FC = () => {
+  return <div>sale</div>;
+};
+
+export default Sale;
